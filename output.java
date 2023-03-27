f3fc9367d7a9ee5b53c59e1b25fc390881cde822
Asmahan_Rashed <65909593+Asmahan-Rashed@users.noreply.github.com>
2023-03-27 21:21:41 +0200

    Updated 1

--- output.java
+++ output.java
@@ -1,12 +1,18 @@
-public class input {
-    int main(){//block number 1
-
-        boolean x=true,z=true;
-        if(x==true){//block number 2
-
-            if(z==true){//block number 3
-
+class output {
+    public static void main(String[] args)//block number 1
+{
+        int x=1,z=1;
+        if(x==1)//block number 2
+{
+            if(z==0)//block number 3
+{
                 print();
+            }
+        }
+        if (x==1) //block number 4
+{
+            if (z==0) //block number 5
+{
             }
         }
     }
